332dd9b911fc0fbc5720e8b548a407b16318a025
MrKrip <45828333+MrKrip@users.noreply.github.com>
2021-11-14 14:05:19 +0200

    Init

---
+++ Lab1/Lab1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Lab1
+    {
+        public void Lab1_1(string Context)
+        {
+            Regex regex = new Regex(@"(^\w+) \w+");
+            String ascii = "";
+
+            for (int i = 0; i < Context.Length; i += 2)
+            {
+
+                String part = Context.Substring(i, 2);
+
+                char ch = (char)Convert.ToInt32(part, 16); ;
+
+                ascii = ascii + ch;
+            }
+            for (int i = 0; i < 255; i++)
+            {
+
+                byte[] output = new byte[ascii.Length];
+                for (int c = 0; c < ascii.Length; c++)
+                {
+                    output[c] = (byte)(ascii[c] ^ Convert.ToByte(i));
+                }
+
+                string dexored = Encoding.ASCII.GetString(output);
+                if (regex.IsMatch(dexored))
+                {
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine(i);
+                    Console.WriteLine();
+                    Console.WriteLine(dexored);                    
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Сесурити\";
+            string Context = File.ReadAllText(path+ "Lab1_1.txt");
+            Lab1 lab1 = new Lab1();
+            lab1.Lab1_1(Context);
+        }
+    }
+}
